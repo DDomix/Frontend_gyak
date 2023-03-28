ba19f773cf312c8f0e3f3a4fbe4ff5f221ef6341
DDomix <dominik.papp1010@gmail.com>
2023-03-28 09:08:05 +0200

    done

--- src/idopont.tsx
+++ src/idopont.tsx
@@ -8,9 +8,12 @@ export default class Idopont extends Component{
     render(): ReactNode {
         return<div className="kapcs">
             <form onSubmit={this.submit}>
-                <input type="text" required  placeholder="Név" className="lol"/><br/>
-                <input type="date" className="lol"/><br/>
-                <textarea  required placeholder="Leírás" className="lol"/><br/>
+                <input type="text" required  placeholder="Név" className="lol"/>
+                <br/>
+                <input type="date" className="lol"/>
+                <br/>
+                <textarea  required placeholder="Leírás" className="lol"/>
+                <br/>
                 <button className="lol">Küldés</button>
             </form>
         </div>
